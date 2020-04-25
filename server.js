808c89fa45df338d5176c0556f904f61b69e44ad
LindseyLowther44 <lindseymlowther@gmail.com>
2020-04-25 00:53:27 -0500

    tweeking small bugs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 var app = express();
 
 var PORT = process.env.PORT || 8080;
@@ -11,8 +12,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("app/public"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
